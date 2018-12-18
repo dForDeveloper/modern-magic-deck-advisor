d11529c00aa616744e4c1d9ba25001544902b9df
Jeo D <e8gea6w5z8id@opayq.com>
2018-12-18 14:42:03 -0700

    Stub out Aside component

---
+++ src/Aside/Aside.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+class Aside extends Component {
+  constructor() {
+    super();
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return(
+      <aside>
+        {/* <Controls /> */}
+        {/* <AsideBody /> */}
+      </aside>
+    );
+  }
+}
+
+export default Aside;
