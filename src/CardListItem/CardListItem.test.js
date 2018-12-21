ec9f96381c53d5458136a1e2686e45e0aed21bc8
Hillary Stewart <hillarystewart4427@gmail.com>
2018-12-20 18:34:26 -0700

    Write test for third conditional of updateListItem

--- src/CardListItem/CardListItem.test.js
+++ src/CardListItem/CardListItem.test.js
@@ -1,69 +1,90 @@
-import React from 'react';
-import CardListItem from './CardListItem';
-import { shallow } from 'enzyme';
+import React from "react";
+import CardListItem from "./CardListItem";
+import { shallow } from "enzyme";
 
-const cardName = 'Blood Moon';
+const cardName = "Blood Moon";
 const index = 0;
-const userCardsData = [{
-  cardName: 'Blood Moon',
-  cardCount: 1
-}];
+const userCardsData = [
+  {
+    cardName: "Blood Moon",
+    cardCount: 1
+  }
+];
 const removeListItemMock = jest.fn();
 const setCardCountMock = jest.fn();
+// const decreaseCardCount = jest.fn();
 
-describe('CardListItem', () => {
+describe("CardListItem", () => {
   let wrapper;
   beforeEach(() => {
     wrapper = shallow(
-      <CardListItem 
+      <CardListItem
         cardName={cardName}
         cardIndex={index}
         key={cardName}
         removeListItem={removeListItemMock}
-        setCardCount={setCardCountMock} 
-        userCardsData={userCardsData}/>
-    )
-  })
+        setCardCount={setCardCountMock}
+        userCardsData={userCardsData}
+      />
+    );
+  });
 
-  it('should match the snapshot', () => {
+  it("should match the snapshot", () => {
     expect(wrapper).toMatchSnapshot();
   });
 
-  it('should call removeListItem when the trash-can is clicked', () => {
-    wrapper.find('.cardlist--item').simulate('click', {
+  it("should call removeListItem when the trash-can is clicked", () => {
+    wrapper.find(".cardlist--item").simulate("click", {
       target: {
         classList: {
-          contains: (className) => {
-            if (className === 'fa-trash-alt') {
+          contains: className => {
+            if (className === "fa-trash-alt") {
               return true;
-            } else {
-              return false;
             }
           }
         },
         closest: () => {
-          return {id: '0'}
+          return { id: "0" };
         }
       }
-    })
+    });
     expect(removeListItemMock).toBeCalled();
-  })
+  });
 
-  it('should increment state count when plus sign is clicked', () => {
-    expect(wrapper.state('count')).toEqual(1);
-    wrapper.find('.cardlist--item').simulate('click', {
+  it("should increment state count when plus sign is clicked", () => {
+    expect(wrapper.state("count")).toEqual(1);
+    wrapper.find(".cardlist--item").simulate("click", {
       target: {
         classList: {
-          contains: (className) => {
-            if (className === 'fa-plus') {
+          contains: className => {
+            if (className === "fa-plus") {
               return true;
-            } else {
-              return false;
             }
           }
         }
       }
-    })
-    expect(wrapper.state('count')).toEqual(2);
-  })
-})
+    });
+    expect(wrapper.state("count")).toEqual(2);
+  });
+
+  it("should decrement state count when minus sign is clicked", () => {
+    wrapper.instance().increaseCardCount({userCardsData, cardName})
+    expect(wrapper.state("count")).toEqual(2);
+    wrapper.find(".cardlist--item").simulate("click", {
+      target: {
+        classList: {
+          contains: className => {
+            if (className === "fa-minus") {
+              return true;
+            } 
+          }
+        }
+      }
+    });
+    expect(wrapper.state("count")).toEqual(1);
+  });
+
+  
+
+
+});
