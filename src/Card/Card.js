91b30a103dc2dc03492265143a1924ee9930149b
Hillary Stewart <hillarystewart4427@gmail.com>
2018-12-20 19:13:38 -0700

    Add functionality to display card count in card area

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,26 +1,26 @@
-import React, { Component } from 'react';
-import './Card.css'
+import React, { Component } from "react";
+import "./Card.css";
 
 class Card extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-
-    };
+    this.state = {};
   }
 
-  render(props) { 
-    return(
-        <article className='card--container'>
-          <img className="card--image"
-                src={this.props.userCard.imageSource}
-                alt={this.props.userCard.cardName} />
-          <div className='counter--container'>
-            <h1 className='card--counter'>1x</h1>   
-          </div>
-        </article>
-      );
-    }
+  render(props) {
+    return (
+      <article className="card--container">
+        <img
+          className="card--image"
+          src={this.props.userCard.imageSource}
+          alt={this.props.userCard.cardName}
+        />
+        <div className="counter--container">
+          <h1 className="card--counter">{this.props.userCard.cardCount}x</h1>
+        </div>
+      </article>
+    );
   }
-  
-  export default Card; 
+}
+
+export default Card;
--- src/CardArea/CardArea.js
+++ src/CardArea/CardArea.js
@@ -1,31 +1,23 @@
-import React, {Component} from 'react';
-import './CardArea.css';
-import Card from '../Card/Card.js'
-import Header from '../Header/Header.js'
-
+import React, { Component } from "react";
+import "./CardArea.css";
+import Card from "../Card/Card.js";
+import Header from "../Header/Header.js";
 
 class CardArea extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-
-    }
+    this.state = {};
   }
 
   render(props) {
-    return(
+    return (
       <div className="card-area">
-        <Header/>
-        {
-          this.props.userCardsData.map(userCard => {
-            return <Card 
-                      userCard={userCard}
-                      key={userCard.cardName}
-            />
-          })
-        }
+        <Header />
+        {this.props.userCardsData.map(userCard => {
+          return <Card userCard={userCard} key={userCard.cardName} />;
+        })}
       </div>
-    )
+    );
   }
 }
 
