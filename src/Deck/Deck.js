3a900aded6d5486acb0fb95de2d05933e6cfbf43
Hillary Stewart <hillarystewart4427@gmail.com>
2018-12-27 21:38:20 -0700

    Add button to start functionality to be able to expand the decks

--- src/Deck/Deck.js
+++ src/Deck/Deck.js
@@ -6,16 +6,26 @@ class Deck extends Component {
     super(props);
     this.state = {};
   }
+
+
+  handleClick = (event) => {
+    let target = event.target.className;
+  }
   
   render(props) {
     return (
       <article className="deck--container">
-        <h2 className="deck--name">{this.props.userDeck.deckName}</h2>
+        <p className="deck--name">{this.props.userDeck.deckName}</p>
         <img
           className="deck--image"
           src="https://musingsofalifelongnerd.files.wordpress.com/2015/01/mtg-card-back.jpg"
           alt={this.props.userDeck.deckName}
         />
+        <button onClick={this.handleClick}
+                className={this.props.userDeck.deckName}
+                >
+          {this.props.userDeck.cards.length} cards
+        </button>
         <div className="price--container">
           <h3 className="price--value">${this.props.userDeck.price}</h3>
         </div>
