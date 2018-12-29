cb3d61d9899e66d72c0b879ec6ae3a00ea8c7e90
Dylan Hofmann <dylan.hofmann@gmail.com>
2018-12-28 18:03:07 -0700

    Initial commit for wish list

---
+++ src/WishListItem/WishListItem.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import "./WishListItem.scss";
+
+class WishListItem extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  remoreWishListItem = event => {
+    this.props.removeWishListItem(event.target.closest("li").id)
+  }
+
+  render() {
+    return(
+      <li id={this.props.cardIndex}>
+        {this.props.cardName}
+        <i 
+          onClick={this.removeWishListItem}
+          className="far fa-trash-alt" />
+      </li>
+    )
+  }
+}
+
+export default WishListItem;
