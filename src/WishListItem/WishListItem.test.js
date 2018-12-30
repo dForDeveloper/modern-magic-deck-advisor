50b6dc36fba5280beac433341377b15167e91b92
Jeo D <e8gea6w5z8id@opayq.com>
2018-12-30 10:47:54 -0800

    Write tests for new WishListItem methods

--- src/WishListItem/WishListItem.test.js
+++ src/WishListItem/WishListItem.test.js
@@ -4,7 +4,9 @@ import { shallow } from 'enzyme';
 
 const cardName = "Blood Moon"
 const index = 0;
-const removeWishListItemMock = jest.fn();
+const wishListCount = 2;
+const wishList = [{ cardName: 'Blood Moon', wishListCount: 2 }];
+const saveArrayMock = jest.fn();
 
 describe("WishListItem", () => {
   let wrapper;
@@ -14,23 +16,40 @@ describe("WishListItem", () => {
       <WishListItem
         cardName={cardName}
         cardIndex={index}
+        wishListCount={wishListCount}
         key={cardName}
-        removeWishListItem={removeWishListItemMock}/>
-    )
-  })
+        wishList={wishList}
+        saveArray={saveArrayMock}/>
+    );
+  });
 
   it('should match the snapshot', () => {
     expect(wrapper).toMatchSnapshot();
   });
 
-  it('should call removeWishListItem method from props when removeListItem is called', () => {
-    wrapper.find(".fa-trash-alt").simulate("click", {
-      target: {
-        closest: () => {
-          return { id: 0 }
-        }
-      }
-    })
-    expect(removeWishListItemMock).toBeCalled();
+  it('should call removeWishListItem when the trash-can is clicked', () => {
+    wrapper.find('.fa-trash-alt').simulate('click');
+    expect(saveArrayMock).toBeCalled();
   });
-})
+
+  it(`should call updateWishList with the incremented count
+  when the plus sign is clicked`, () => {
+  wrapper.instance().updateWishList = jest.fn();
+  wrapper.update();
+  wrapper.find('.fa-plus').simulate('click');
+  expect(wrapper.instance().updateWishList).toBeCalledWith(3);
+  });
+
+  it(`should call updateWishList with the decremented count
+    when the plus sign is clicked`, () => {
+    wrapper.instance().updateWishList = jest.fn();
+    wrapper.update();
+    wrapper.find('.fa-minus').simulate('click');
+    expect(wrapper.instance().updateWishList).toBeCalledWith(1);
+  });
+
+  it('should call saveArray when updateWishList is called', () => {
+    wrapper.instance().updateWishList(3);
+    expect(saveArrayMock).toBeCalled();
+  });
+});
