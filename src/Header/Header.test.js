21654ef7a2734849f4d9ab192470718c36e451e2
Dylan Hofmann <dylan.hofmann@gmail.com>
2018-12-31 18:02:13 -0700

    Change method to call set Card Area View instead of reload page

--- src/Header/Header.test.js
+++ src/Header/Header.test.js
@@ -2,16 +2,21 @@ import React from 'react';
 import Header from './Header';
 import { shallow } from 'enzyme';
 
+const setAsideViewMock = jest.fn();
+
 describe('Header', () => {
   let wrapper;
 
   beforeEach(() => {
     wrapper = shallow(
-      <Header/>
+      <Header
+        setAsideView={setAsideViewMock}/>
     )
   });
 
   it('should match the snapshot', () => {
     expect(wrapper).toMatchSnapshot();
   }); 
+
+  it('should call setAsideView and return to Cards when My Cards is clicked')
 }) 
