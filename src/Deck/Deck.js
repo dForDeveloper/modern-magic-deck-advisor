0239b3997a7a9025be0f903dd6e218038a2fc657
Hillary Stewart <hillarystewart4427@gmail.com>
2019-01-01 20:03:32 -0700

    Restructure the elements in the render of the decks

--- src/Deck/Deck.js
+++ src/Deck/Deck.js
@@ -3,30 +3,30 @@ import '../styles/main.scss';
 
 class Deck extends Component {
   handleClick = () => {
-    this.props.setCardAreaView('expandedDeck') 
+    this.props.setCardAreaView('expandedDeck');
     this.props.expandDeck(this.props.userDeck);
-  }
+  };
 
   addToFaveDecks = () => {
     this.props.addToFaveDecks(this.props.userDeck);
-  }
-  
+  };
+
   render() {
     return (
       <article className="deck--container">
         <h2 className="deck--name">{this.props.userDeck.deckName}</h2>
+        <h3 className="price--value">${this.props.userDeck.price}</h3>
         <img
           className="deck--image"
           src="https://musingsofalifelongnerd.files.wordpress.com/2015/01/mtg-card-back.jpg"
           alt={this.props.userDeck.deckName}
         />
-        <button onClick={this.handleClick}>
-                {this.props.userDeck.cards.length} cards
-        </button>
-        <div className="price--container">
-          <h3 className="price--value">${this.props.userDeck.price}</h3>
-          <button onClick={this.addToFaveDecks}>
-            Add to Favorite Decks
+        <div className="deck--divFlex">
+          <button className="deck--button" onClick={this.addToFaveDecks}>
+            Add to My Favorites
+          </button>
+          <button className="deck--card-button" onClick={this.handleClick}>
+            Show all cards
           </button>
         </div>
       </article>
