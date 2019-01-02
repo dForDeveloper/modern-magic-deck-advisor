fc04c5a8f47835157bd8d54866f0cda9924fd4ec
Hillary Stewart <hillarystewart4427@gmail.com>
2019-01-02 16:06:04 -0700

    Update styling for aside and list items

--- src/Controls/Controls.js
+++ src/Controls/Controls.js
@@ -61,24 +61,26 @@ class Controls extends Component {
   render() {
     let errorMessage;
     if (this.state.isInvalidCardName) {
-      errorMessage = "The card name you entered is not in the database"
+      errorMessage = "The card name you entered is not in the database."
     } else if (this.state.hasDuplicates) {
-      errorMessage = "This card is already in your list"
+      errorMessage = "This card is already in your list."
     }
     return(
       <form>
-        <input
-          type="text"
-          className="controls--input"
-          onChange={this.updateCardName}
-          placeholder="Enter cards you own"
-          ref={this.textInput}>
-        </input>
-        <button
-          className="controls--button"
-          onClick={this.submitCard}>
-            Add Card
-        </button>
+        <div className="controls--div">
+          <input
+            type="text"
+            className="controls--input"
+            onChange={this.updateCardName}
+            placeholder="Enter cards you own"
+            ref={this.textInput}>
+          </input>
+          <button
+            className="controls--button"
+            onClick={this.submitCard}>
+              Add Card
+          </button>
+        </div> 
         <p className="controls--error">{errorMessage}</p>
       </form>
     )
