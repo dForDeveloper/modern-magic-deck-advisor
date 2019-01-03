9df88fe3515ecbd175a8ee6d640ad5a76ef71f50
Hillary Stewart <hillarystewart4427@gmail.com>
2019-01-02 17:01:25 -0700

    Update styling for cards in cardArea

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,20 +1,23 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import '../styles/main.scss';
 
 class Card extends Component {
   setPopUpData = () => {
     this.props.displayPopUp(this.props.card);
-  }
+  };
 
   addToWishlist = () => {
     this.props.addToWishlist(this.props.card);
-  }
+  };
 
   render() {
     return (
       <article className="card--container">
-        {this.props.cardAreaView === "myCardList" && (
+        {this.props.cardAreaView === 'myCardList' && (
           <div>
+            <div className="card--header-div">
+              <h2>{this.props.card.cardName}</h2>
+            </div>
             <img
               className="card--image"
               src={this.props.card.imageSource}
@@ -22,14 +25,15 @@ class Card extends Component {
               onClick={this.setPopUpData}
             />
             <div className="card--counter-container">
-              <h1 className="card--counter">
-                {this.props.card.cardCount}X
-              </h1>
+              <p className="card--counter">{this.props.card.cardCount} in My Cards</p>
             </div>
           </div>
         )}
-        {this.props.cardAreaView === "expandedDeck" && (
+        {this.props.cardAreaView === 'expandedDeck' && (
           <div>
+            <div className="card--header-div">
+              <h2>{this.props.card.cardName}</h2>
+            </div>
             <img
               className="card--image"
               src={this.props.card.imageSource}
@@ -39,29 +43,33 @@ class Card extends Component {
             <div className="counter--container">
               <p>{this.props.card.price}</p>
               <p>
-                {
-                  `You have ${this.props.card.userCount}
-                   of ${this.props.card.requiredCount}`
-                }
+                {`You have ${this.props.card.userCount}
+                   of ${this.props.card.requiredCount}`}
               </p>
-              <button onClick={this.addToWishlist}>
-                Add to Wishlist
-              </button>
+              <button onClick={this.addToWishlist}>Add to Wishlist</button>
             </div>
           </div>
         )}
         {this.props.cardAreaView === 'wishList' && (
           <div>
-          <img
-            className="card--image"
-            src={this.props.card.imageSource}
-            alt={this.props.card.cardName}
-            onClick={this.setPopUpData}
-          />
-          <div>
-            <p>Total Cost - ${(this.props.card.price * this.props.card.wishListCount).toFixed(2)}</p>
+            <div className="card--header-div">
+              <h2>{this.props.card.cardName}</h2>
+            </div>
+            <img
+              className="card--image"
+              src={this.props.card.imageSource}
+              alt={this.props.card.cardName}
+              onClick={this.setPopUpData}
+            />
+            <div>
+              <p>
+                Total Cost - $
+                {(
+                  this.props.card.price * this.props.card.wishListCount
+                ).toFixed(2)}
+              </p>
+            </div>
           </div>
-        </div>
         )}
       </article>
     );
