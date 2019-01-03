ec26c074124b196e8228ea9b8a543236191527f5
Hillary Stewart <hillarystewart4427@gmail.com>
2019-01-02 18:04:35 -0700

    Update styling for Deck component

--- src/Deck/Deck.js
+++ src/Deck/Deck.js
@@ -21,32 +21,25 @@ class Deck extends Component {
 
   render() {
     const images = this.getDeckImage();
-    return (
-      <article className="deck--container">
+    return <article className="deck--container">
         <div>
-          <h2 className="deck--name">{this.props.userDeck.deckName}</h2>
-          <h3 className="price--value">${this.props.userDeck.price}</h3>
+          <div className="deck--header-div">
+            <h2 className="deck--name">{this.props.userDeck.deckName}</h2>
+          </div>
+            <h3 className="price--value">${this.props.userDeck.price}</h3>
           <div className="deck--signature">
             {images.map(image => {
-              return (
-                <img
-                  className="deck--image"
-                  src={image}
-                  alt={this.props.userDeck.deckName}
-                  key={image}/>
-              )
+              return <img className="deck--image" src={image} alt={this.props.userDeck.deckName} key={image} />;
             })}
           </div>
-          {this.props.cardAreaView === 'compareDecks' && (
-          <button className="deck--button" onClick={this.addToFaveDecks}>
-            Save Deck
-          </button>)}
+          {this.props.cardAreaView === 'compareDecks' && <button className="deck--button" onClick={this.addToFaveDecks}>
+              Save Deck
+            </button>}
           <button className="deck--card-button" onClick={this.handleClick}>
             Show all cards
           </button>
         </div>
-      </article>
-    );
+      </article>;
   }
 }
 
